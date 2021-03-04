a6b6dfc291bdd8c252d0bb744e443e16a70f3f07
TRiG <gerysh07@gmail.com>
2021-03-04 18:41:27 +0300

    lesson 4

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 # Программа сервера
 from socket import *
 import time
-
+from contextlib import closing
 import click
 
 
@@ -10,16 +10,23 @@ import click
 @click.option('--port', default=8888, prompt='port',
               help='port server.')
 def server_answ(addr, port):
-    s = socket(AF_INET, SOCK_STREAM)
-    s.bind((addr, port))
-    print('Server start')
-    s.listen(5)
-    client, addr = s.accept()
-    data = client.recv(1000000)
-    print('Сообщение: ', data.decode('utf-8'), ', было отправлено клиентом: ', addr)
-    timestr = time.ctime(time.time()) + "\n"
-    client.send(timestr.encode('ascii'))
-    client.close()
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.bind((addr, port))
+        print('Server start')
+        s.listen(5)
+        while True:
+            client, addr = s.accept()
+            with closing(client) as cl:
+                data = cl.recv(1000000)
+                print('Сообщение: ', data.decode('utf-8'), ', было отправлено клиентом: ', addr)
+                timestr = time.ctime(time.time()) + "\n"
+                print(data.decode('utf-8'))
+                if data.decode('utf-8') == 'message1':
+                    cl.send('reply1'.encode('utf-8'))
+                elif data.decode('utf-8') == 'message2':
+                    cl.send('reply2'.encode('utf-8'))
+                else:
+                    cl.send(timestr.encode('ascii'))
 
 
 while True:
